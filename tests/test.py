dec06d5e7f6c690faf02fcabc167944bd91b69fa
Erfan Azad <erfan.azad@tiliter.com>
2019-11-07 15:45:42 +0100

    loader method

--- tests/test.py
+++ tests/test.py
@@ -7,11 +7,13 @@ class TestMethods(unittest.TestCase):
 
     def test_load_image_with_border(self):
         filename = "../data/head.pgm"
-        img = Loader.load_image_with_mirrored_border(filename, cv2.IMREAD_GRAYSCALE)
-        cv2.imshow('image', img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        img = Loader.load_image_with_mirrored_border(filename, 0, cv2.IMREAD_GRAYSCALE)
+        #
+        # cv2.imshow('image', img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
     unittest.main()
+
