187cd1005c794a0693e39b5a5b86b4d37b355395
mikel <700630@celes.unizar.es>
2019-11-07 23:46:18 +0100

    nonlinear diffusion finished

--- loader.py
+++ loader.py
@@ -36,63 +36,62 @@ class Loader:
         return (up - un) * 0.5
 
     def diff_between_edges(self, side_pix, pix):
-        return (side_pix + pix) * 0.5
+        return (side_pix + pix)*0.5
 
     # @njit
     def nonlinear_isotropic_diffusivity(self, filename, border_width, param, tau, iter):
         it = 0
         img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
         mirrored_img = self.load_image_with_mirrored_border(img, border_width)
-        cv2.imwrite('foto.jpg', mirrored_img)
+        #cv2.imwrite('foto.jpg', mirrored_img)
 
-        img_next = np.zeros((img.shape))
+        img_next = np.zeros((mirrored_img.shape))
         while (it < iter):
-            d_x_u = np.zeros((img.shape))
-            d_y_u = np.zeros((img.shape))
-            # sqr_grad = np.zeros((img.shape))
-            for i in range(img.shape[0]):
-                for j in range(img.shape[1]):
-                    d_x_u[i, j] = self.spatial_derivative(mirrored_img[border_width + i + 1, j + border_width],
-                                                          mirrored_img[border_width + i - 1, border_width + j])
-                    d_y_u[i, j] = self.spatial_derivative(mirrored_img[border_width + i, border_width + j + 1],
-                                                          mirrored_img[border_width + i, border_width + j - 1])
-
-            abs_sqr_grad = d_x_u * d_x_u + d_y_u * d_y_u
+            d_x_u = np.zeros((mirrored_img.shape))
+            d_y_u = np.zeros((mirrored_img.shape))
+            for i in range(1,(mirrored_img.shape[0]-1),1):
+                for j in range(1,(mirrored_img.shape[1]-1),1):
+                    d_x_u[i, j] = self.spatial_derivative(mirrored_img[i + 1, j],
+                                                          mirrored_img[i - 1,  j])
+                    d_y_u[i, j] = self.spatial_derivative(mirrored_img[i,  j + 1],
+                                                          mirrored_img[i,  j - 1])
+            abs_sqr_grad = np.multiply(d_x_u,d_x_u) + np.multiply(d_y_u,d_y_u)
             discret_grad = self.exponential_diffusivity(abs_sqr_grad, param)
-            diff_left = np.zeros((img.shape))
-            diff_right = np.zeros((img.shape))
-            diff_buttom = np.zeros((img.shape))
-            diff_top = np.zeros((img.shape))
+            print('The shape of the mirrored imge is: ' + str(mirrored_img.shape))
+            print('The shape of the abs_squared_grad is : ' + str(abs_sqr_grad.shape))
+            print('The shape of the discretized gradient is : '+str(discret_grad.shape))
+            diff_left = np.zeros((mirrored_img.shape))
+            diff_right = np.zeros((mirrored_img.shape))
+            diff_buttom = np.zeros((mirrored_img.shape))
+            diff_top = np.zeros((mirrored_img.shape))
 
-            for i in range(img.shape[0]):
-                for j in range(img.shape[1]):
-                    diff_buttom[i, j] = self.diff_between_edges(mirrored_img[border_width + i, border_width + j + 1],
-                                                                mirrored_img[border_width + i, border_width + j])
-                    diff_top[i, j] = self.diff_between_edges(mirrored_img[border_width + i, border_width + j - 1],
-                                                             mirrored_img[border_width + i, border_width + j])
-                    diff_right[i, j] = self.diff_between_edges(mirrored_img[border_width + i + 1, border_width + j],
-                                                               mirrored_img[border_width + i, border_width + j])
-                    diff_left[i, j] = self.diff_between_edges(mirrored_img[border_width + i - 1, border_width + j],
-                                                              mirrored_img[border_width + i, border_width + j])
+            for i in range(1, (mirrored_img.shape[0] - 1), 1):
+                for j in range(1, (mirrored_img.shape[1] - 1), 1):
+                    diff_buttom[i, j] = self.diff_between_edges(discret_grad[i, j + 1],
+                                                                discret_grad[ i,  j])
+                    diff_top[i, j] = self.diff_between_edges(discret_grad[ i,  j],
+                                                             discret_grad[ i,  j-1])
+                    diff_right[i, j] = self.diff_between_edges(discret_grad[ i + 1,  j],
+                                                               discret_grad[ i,  j])
+                    diff_left[i, j] = self.diff_between_edges(discret_grad[ i ,  j],
+                                                              discret_grad[ i - 1,  j])
 
-            for i in range(img.shape[0]):
-                for j in range(img.shape[1]):
+            for i in range(1, (mirrored_img.shape[0] - 1), 1):
+                for j in range(1, (mirrored_img.shape[1] - 1), 1):
                     img_next[i, j] = (
                                 (1 - tau * (diff_buttom[i, j] + diff_top[i, j] + diff_right[i, j] + diff_left[i, j])) *
-                                img[i, j] +
+                                mirrored_img[i, j] +
                                 tau * (diff_buttom[i, j] * mirrored_img[i, j + 1] + diff_top[i, j] * mirrored_img[
                             i, j - 1] +
                                        diff_right[i, j] * mirrored_img[i + 1, j] + diff_left[i, j] * mirrored_img[
                                            i - 1, j]))
-            img = img_next
+            mirrored_img = img_next
             it = it + 1
-        return img
+        return mirrored_img
 
 
-filename = "../data/Boats.pgm"
+filename = "../data/BoatsNoise10.pgm"
 loader_obj = Loader()
-result = loader_obj.nonlinear_isotropic_diffusivity(filename, 10, 50, 0.1, 2)
-print(result)
-cv2.imshow('image', result)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+result = loader_obj.nonlinear_isotropic_diffusivity(filename, 5, 30, 0.001, 5)
+cv2.imwrite('foto_denoised_lambda=30_t=0.1.jpg', result)
+
