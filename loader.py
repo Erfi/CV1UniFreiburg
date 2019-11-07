fixed the image loader scaling issue

loader.py
@@ -19,7 +19,7 @@ class Loader:
         top_border = np.flip(img[0:border_width, :], axis=0)
         bottom_border = np.flip(img[rows - border_width: rows, :], axis=0)
 
-        result = np.zeros((rows + 2 * border_width, cols + 2 * border_width))
+        result = np.zeros((rows + 2 * border_width, cols + 2 * border_width), np.uint8)
         result[border_width: rows + border_width, border_width:cols + border_width] = img
         result[border_width: rows + border_width, 0: border_width] = left_border
         result[border_width: rows + border_width, cols + border_width: cols + 2 * border_width] = right_border