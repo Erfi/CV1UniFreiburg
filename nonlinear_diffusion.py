d4f35e1ba0587af7af8e11f711756808c788b48f
Saurav Shanu <saurav.1869@gmail.com>
2019-11-08 00:23:05 +0100

    reverting index changes.

--- nonlinear_diffusion.py
+++ nonlinear_diffusion.py
@@ -1,62 +1,61 @@
-from numba import njit
+from numba import njit, cuda
 import cv2
 import numpy as np
 
 from loader import Loader
 
 
-@njit
-def calculate_diffusivity(img_obj, org_img_shape, variance):
-    x, y = org_img_shape
+@njit(parallel=True, nogil=True)
+def calculate_diffusivity(img_obj, shape, variance):
+    x, y = shape
     diffusivity_matrix = np.zeros((x+2, y+2))
     # hard-coding ahead :(
-    for i in range(1, x + 3):
-        for j in range(1, y + 3):
+    for i in range(x + 1):
+        for j in range(y + 1):
             x_derivative = (img_obj[i+1, j] - img_obj[i-1, j])/2
             y_derivative = (img_obj[i, j+1] - img_obj[i, j-1])/2
             grad_square = x_derivative ** 2 + y_derivative ** 2
-            diffusivity_matrix[i-1][j-1] = np.e ** - (grad_square / variance)
+            diffusivity_matrix[i][j] = np.e ** - (grad_square / variance)
+
     # print(diffusivity_matrix.shape)
     return diffusivity_matrix
 
 
-@njit
-def diffuse(img_obj, diffusivity_matrix, tau, org_img_shape):
-    x, y = org_img_shape
-    new_img_obj = np.zeros(org_img_shape)
+@njit(parallel=True, nogil=True)
+def diffuse(img_obj, diffusivity_matrix, tau):
+    x, y = diffusivity_matrix.shape
+    new_img_obj = np.zeros(diffusivity_matrix.shape)
     # hard-coding ahead :(
-    # index assignments are really crazy.
-    for i in range(1, x+1):
-        for j in range(1, y+1):
+    for i in range(x - 1):
+        for j in range(y - 1):
             diffusivity_i_half_pos = (diffusivity_matrix[i + 1][j] + diffusivity_matrix[i][j]) / 2
             diffusivity_i_half_neg = (diffusivity_matrix[i - 1][j] + diffusivity_matrix[i][j]) / 2
             diffusivity_j_half_pos = (diffusivity_matrix[i][j + 1] + diffusivity_matrix[i][j]) / 2
             diffusivity_j_half_neg = (diffusivity_matrix[i][j - 1] + diffusivity_matrix[i][j]) / 2
-            central_pixel_contrib = (1 - tau * (diffusivity_i_half_pos + diffusivity_i_half_neg + diffusivity_j_half_pos + diffusivity_j_half_neg)) * img_obj[i+1][j+1]
-            neighbour_pixel_contrib = tau * (diffusivity_i_half_pos * img_obj[i+2][j+1] +
-                                             diffusivity_i_half_neg * img_obj[i][j+1] +
-                                             diffusivity_j_half_pos * img_obj[i+1][j+2] +
-                                             diffusivity_j_half_neg * img_obj[i+1][j])
-            new_img_obj[i - 1][j - 1] = central_pixel_contrib + neighbour_pixel_contrib
+            central_pixel_contrib = (1 - tau * (diffusivity_i_half_pos + diffusivity_i_half_neg + diffusivity_j_half_pos + diffusivity_j_half_neg)) * img_obj[i][j]
+            neighbour_pixel_contrib = tau * (diffusivity_i_half_pos * img_obj[i+1][j] +
+                                             diffusivity_i_half_neg * img_obj[i-1][j] +
+                                             diffusivity_j_half_pos * img_obj[i][j+1] +
+                                             diffusivity_j_half_neg * img_obj[i][j-1])
+            new_img_obj[i, j] = central_pixel_contrib + neighbour_pixel_contrib
     return new_img_obj
 
 
-# @njit
-def nonlinear_isotropic_diffusion(img_obj, org_img_shape):
-    steps = 30
+def nonlinear_isotropic_diffusion(img_obj, shape):
+    steps = 100
     variance = steps * 2
-    variance = 50
-    tau = 0.025
+    tau = 0.05
     for i in range(steps):
-        diffusivity_matrix = calculate_diffusivity(img_obj, org_img_shape, variance)
-        new_img_obj = diffuse(img_obj, diffusivity_matrix, tau, org_img_shape)
-        img_obj = Loader.load_image_with_mirrored_border(new_img_obj, 2)
-    return new_img_obj
+        diffusivity_matrix = calculate_diffusivity(img_obj, shape, 50)
+        new_img_obj = diffuse(img_obj, diffusivity_matrix, tau)
+        # i dont know why i did it.
+        img_obj = new_img_obj
+    return img_obj
 
 
 img_obj = cv2.imread("data/BoatsNoise10.pgm", cv2.IMREAD_GRAYSCALE)
-org_img_shape = img_obj.shape
+shape = img_obj.shape
 img_obj = Loader.load_image_with_mirrored_border(img_obj, 2)
 # print(img_obj.shape)
-img_obj = nonlinear_isotropic_diffusion(img_obj, org_img_shape)
+img_obj = nonlinear_isotropic_diffusion(img_obj, shape)
 cv2.imwrite("data/BoatsProcessed.jpeg", img_obj)
