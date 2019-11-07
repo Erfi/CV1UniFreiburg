b4c3223b1ce8b44c1dede9c3dedec5f65f686521
Saurav Shanu <saurav.1869@gmail.com>
2019-11-07 18:06:42 +0100

    nonlinear diffusion finished

--- loader.py
+++ loader.py
@@ -8,18 +8,16 @@ import numpy as np
 class Loader:
 
     @staticmethod
-    def load_image_with_mirrored_border(filename, border_width, *args):
-        img = cv2.imread(filename, *args)
+    def load_image_with_mirrored_border(img, border_width):
         if border_width == 0:
             return img
-
         rows, cols = img.shape
         left_border = np.flip(img[:, 0:border_width], axis=1)
         right_border = np.flip(img[:, cols - border_width: cols], axis=1)
         top_border = np.flip(img[0:border_width, :], axis=0)
         bottom_border = np.flip(img[rows - border_width: rows, :], axis=0)
 
-        result = np.zeros((rows + 2 * border_width, cols + 2 * border_width), np.uint8)
+        result = np.zeros((rows + 2 * border_width, cols + 2 * border_width), np.int)
         result[border_width: rows + border_width, border_width:cols + border_width] = img
         result[border_width: rows + border_width, 0: border_width] = left_border
         result[border_width: rows + border_width, cols + border_width: cols + 2 * border_width] = right_border
--- nonlinear_diffusion.py
+++ nonlinear_diffusion.py
@@ -1,15 +1,61 @@
 from numba import njit
+import cv2
 import numpy as np
 
+from loader import Loader
 
-def nonlinear_isotropic_diffusion(img_obj):
-    x, y, _ = img_obj.shape
-    print(x, y)
-    img_obj = mirror_image(img_obj, pixels=5)
-    # for i in range(x):
-    #     for j in range(y):
-    #         pass
-    # return noisy_lena_img
 
-#
-# nonlinear_isotropic_diffusion()
+@njit
+def calculate_diffusivity(img_obj, shape, variance):
+    x, y = shape
+    diffusivity_matrix = np.zeros((x+2, y+2))
+    # hard-coding ahead :(
+    for i in range(x + 1):
+        for j in range(y + 1):
+            x_derivative = (img_obj[i+1, j] - img_obj[i-1, j])/2
+            y_derivative = (img_obj[i, j+1] - img_obj[i, j-1])/2
+            grad_square = x_derivative ** 2 + y_derivative ** 2
+            diffusivity_matrix[i][j] = np.e ** - (grad_square / variance)
+
+    # print(diffusivity_matrix.shape)
+    return diffusivity_matrix
+
+
+@njit
+def diffuse(img_obj, diffusivity_matrix, tau):
+    x, y = diffusivity_matrix.shape
+    new_img_obj = np.zeros(diffusivity_matrix.shape)
+    # hard-coding ahead :(
+    for i in range(x - 1):
+        for j in range(y - 1):
+            diffusivity_i_half_pos = (diffusivity_matrix[i + 1][j] + diffusivity_matrix[i][j]) / 2
+            diffusivity_i_half_neg = (diffusivity_matrix[i - 1][j] + diffusivity_matrix[i][j]) / 2
+            diffusivity_j_half_pos = (diffusivity_matrix[i][j + 1] + diffusivity_matrix[i][j]) / 2
+            diffusivity_j_half_neg = (diffusivity_matrix[i][j - 1] + diffusivity_matrix[i][j]) / 2
+            central_pixel_contrib = (1 - tau * (diffusivity_i_half_pos + diffusivity_i_half_neg + diffusivity_j_half_pos + diffusivity_j_half_neg)) * img_obj[i][j]
+            neighbour_pixel_contrib = tau * (diffusivity_i_half_pos * img_obj[i+1][j] +
+                                             diffusivity_i_half_neg * img_obj[i-1][j] +
+                                             diffusivity_j_half_pos * img_obj[i][j+1] +
+                                             diffusivity_j_half_neg * img_obj[i][j-1])
+            new_img_obj[i, j] = central_pixel_contrib + neighbour_pixel_contrib
+    return new_img_obj
+
+
+def nonlinear_isotropic_diffusion(img_obj, shape):
+    steps = 100
+    variance = steps * 2
+    tau = 0.1
+    for i in range(steps):
+        diffusivity_matrix = calculate_diffusivity(img_obj, shape, 10)
+        new_img_obj = diffuse(img_obj, diffusivity_matrix, tau)
+        # i dont know why i did it.
+        img_obj = new_img_obj
+    return img_obj
+
+
+img_obj = cv2.imread("data/BoatsNoise10.pgm", cv2.IMREAD_GRAYSCALE)
+shape = img_obj.shape
+img_obj = Loader.load_image_with_mirrored_border(img_obj, 2)
+# print(img_obj.shape)
+img_obj = nonlinear_isotropic_diffusion(img_obj, shape)
+cv2.imwrite("data/BoatsProcessed.jpeg", img_obj)
