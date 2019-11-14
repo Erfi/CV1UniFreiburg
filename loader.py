5d0a613bb616a55b5c2256d2871cb9cededcb60f
mikel <700630@celes.unizar.es>
2019-11-14 19:50:38 +0100

    nonlinear diffusion finished

--- loader.py
+++ loader.py
@@ -6,7 +6,7 @@ import numpy as np
 from numba import njit
 
 
-class Loader:
+class Diffuser:
 
     def load_image_with_mirrored_border(self, img, border_width):
 
@@ -38,60 +38,61 @@ class Loader:
     def diff_between_edges(self, side_pix, pix):
         return (side_pix + pix)*0.5
 
-    # @njit
+    #@njit
+    def _grad_diffusivity_matrix(self, mirrored_img, param):
+        d_x_u = np.zeros((mirrored_img.shape))
+        d_y_u = np.zeros((mirrored_img.shape))
+        for i in range(1, (mirrored_img.shape[0] - 1), 1):
+            for j in range(1, (mirrored_img.shape[1] - 1), 1):
+                d_x_u[i, j] = self.spatial_derivative(mirrored_img[i + 1, j],
+                                                      mirrored_img[i - 1, j])
+                d_y_u[i, j] = self.spatial_derivative(mirrored_img[i, j + 1],
+                                                      mirrored_img[i, j - 1])
+        abs_sqr_grad = np.multiply(d_x_u, d_x_u) + np.multiply(d_y_u, d_y_u)
+        discret_grad = self.exponential_diffusivity(abs_sqr_grad, param)
+        return discret_grad
+
+    #@njit
+    def _diffuse(self, discret_grad):
+        diffus_edges = np.zeros((*discret_grad.shape, 4))
+        for i in range(1, (diffus_edges.shape[0] - 1), 1):
+            for j in range(1, (diffus_edges.shape[1] - 1), 1):
+                # --Bottom--
+                diffus_edges[i, j, 0] = self.diff_between_edges(discret_grad[i, j + 1],
+                                                                discret_grad[i, j])
+                # --Top--
+                diffus_edges[i, j, 1] = self.diff_between_edges(discret_grad[i, j],
+                                                                discret_grad[i, j - 1])
+                # --Right--
+                diffus_edges[i, j, 2] = self.diff_between_edges(discret_grad[i + 1, j],
+                                                                discret_grad[i, j])
+                # --Left--
+                diffus_edges[i, j, 3] = self.diff_between_edges(discret_grad[i, j],
+                                                                discret_grad[i - 1, j])
+        return diffus_edges
+
+    #@njit
     def nonlinear_isotropic_diffusivity(self, filename, border_width, param, tau, iter):
-        it = 0
         img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
         mirrored_img = self.load_image_with_mirrored_border(img, border_width)
-        #cv2.imwrite('foto.jpg', mirrored_img)
-
         img_next = np.zeros((mirrored_img.shape))
-        while (it < iter):
-            d_x_u = np.zeros((mirrored_img.shape))
-            d_y_u = np.zeros((mirrored_img.shape))
-            for i in range(1,(mirrored_img.shape[0]-1),1):
-                for j in range(1,(mirrored_img.shape[1]-1),1):
-                    d_x_u[i, j] = self.spatial_derivative(mirrored_img[i + 1, j],
-                                                          mirrored_img[i - 1,  j])
-                    d_y_u[i, j] = self.spatial_derivative(mirrored_img[i,  j + 1],
-                                                          mirrored_img[i,  j - 1])
-            abs_sqr_grad = np.multiply(d_x_u,d_x_u) + np.multiply(d_y_u,d_y_u)
-            discret_grad = self.exponential_diffusivity(abs_sqr_grad, param)
-            print('The shape of the mirrored imge is: ' + str(mirrored_img.shape))
-            print('The shape of the abs_squared_grad is : ' + str(abs_sqr_grad.shape))
-            print('The shape of the discretized gradient is : '+str(discret_grad.shape))
-            diff_left = np.zeros((mirrored_img.shape))
-            diff_right = np.zeros((mirrored_img.shape))
-            diff_buttom = np.zeros((mirrored_img.shape))
-            diff_top = np.zeros((mirrored_img.shape))
-
-            for i in range(1, (mirrored_img.shape[0] - 1), 1):
-                for j in range(1, (mirrored_img.shape[1] - 1), 1):
-                    diff_buttom[i, j] = self.diff_between_edges(discret_grad[i, j + 1],
-                                                                discret_grad[ i,  j])
-                    diff_top[i, j] = self.diff_between_edges(discret_grad[ i,  j],
-                                                             discret_grad[ i,  j-1])
-                    diff_right[i, j] = self.diff_between_edges(discret_grad[ i + 1,  j],
-                                                               discret_grad[ i,  j])
-                    diff_left[i, j] = self.diff_between_edges(discret_grad[ i ,  j],
-                                                              discret_grad[ i - 1,  j])
-
+        for it in range(iter):
+            discret_grad = self._grad_diffusivity_matrix(mirrored_img, param)
+            diffus_edges = self._diffuse(discret_grad)
             for i in range(1, (mirrored_img.shape[0] - 1), 1):
                 for j in range(1, (mirrored_img.shape[1] - 1), 1):
                     img_next[i, j] = (
-                                (1 - tau * (diff_buttom[i, j] + diff_top[i, j] + diff_right[i, j] + diff_left[i, j])) *
+                                (1 - tau * (diffus_edges[i, j, 0] + diffus_edges[i, j, 1] + diffus_edges[i, j, 2] + diffus_edges[i, j, 3])) *
                                 mirrored_img[i, j] +
-                                tau * (diff_buttom[i, j] * mirrored_img[i, j + 1] + diff_top[i, j] * mirrored_img[
+                                tau * (diffus_edges[i, j, 0] * mirrored_img[i, j + 1] + diffus_edges[i, j, 1] * mirrored_img[
                             i, j - 1] +
-                                       diff_right[i, j] * mirrored_img[i + 1, j] + diff_left[i, j] * mirrored_img[
+                                       diffus_edges[i, j, 2] * mirrored_img[i + 1, j] + diffus_edges[i, j, 3] * mirrored_img[
                                            i - 1, j]))
             mirrored_img = img_next
-            it = it + 1
         return mirrored_img
 
 
 filename = "../data/BoatsNoise10.pgm"
-loader_obj = Loader()
-result = loader_obj.nonlinear_isotropic_diffusivity(filename, 5, 30, 0.001, 5)
+diffused_obj = Diffuser()
+result = diffused_obj.nonlinear_isotropic_diffusivity(filename, 5, 30, 0.001, 5)
 cv2.imwrite('foto_denoised_lambda=30_t=0.1.jpg', result)
-
